45c2eaa7a398c713b39d6379f290da18721903d0
zenkovnick <zenkovnick@zenkovnick.(none)>
2013-06-22 14:19:55 +0300

    Devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aconverter::Application.config.secret_token = 'bbe2d8ba87ff6894435dcdbfeea14dd19f3c748511e98289b06062b2bf8bb925560b7decf45507b1925c7e9951ddd62b20cf66ad364623452395139b481018e4'
